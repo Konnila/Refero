48544b6835b8db08ae01a5a3cd26f6553af6f38d
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2012-03-30 13:17:25 +0300

    Basic Spring configuration

---
+++ src/main/java/ohtu/refero/controllers/RootController.java
@@ -0,0 +1,13 @@
+package ohtu.refero.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "index";
+    }
+}
