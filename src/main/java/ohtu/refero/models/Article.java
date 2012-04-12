076acf99dcd59df406aab11bb7eea0af6f64d6e3
Lauri Pulkkinen <lauri.pulkkinen@helsinki.fi>
2012-04-12 20:43:45 +0300

    Article model

--- src/main/java/ohtu/refero/models/Article.java
+++ src/main/java/ohtu/refero/models/Article.java
@@ -4,5 +4,79 @@ import javax.persistence.Entity;
 
 @Entity
 public class Article extends JPAObject {
+    
+    
+    private String author, title, journal;
+    private int volume, number, releaseYear;
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getYear() {
+        return releaseYear;
+    }
+
+    public void setYear(int year) {
+        this.releaseYear = year;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        Article other = (Article) obj;
+        if (this.getId() != other.getId() && (this.getId() == null || !this.getId().equals(other.getId()))) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return author.hashCode();
+    }
 }
