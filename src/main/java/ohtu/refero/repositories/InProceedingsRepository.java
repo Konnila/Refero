807055a942967ab70e02ec8e96c590aae2b4b3dd
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2012-04-18 22:17:34 +0300

    Delete Inproceedings*

--- src/main/java/ohtu/refero/repositories/InProceedingsRepository.java
+++
@@ -1,8 +0,0 @@
-package ohtu.refero.repositories;
-
-import ohtu.refero.models.Inproceedings;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InproceedingsRepository extends JpaRepository<Inproceedings, Long> {
-
-}
