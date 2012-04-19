ba4b4bccd7a4017542b981c831a0c3bcd3783375
Konnila <toni.konnila@helsinki.fi>
2012-04-19 22:08:13 +0300

    fixed

--- src/main/java/ohtu/refero/models/Reference.java
+++ src/main/java/ohtu/refero/models/Reference.java
@@ -20,15 +20,15 @@ public abstract class Reference extends JPAObject {
     private String publisher;
     private String address;
     
-    private String referenceID;
+//    private String referenceID;
 
-    public String getReferenceID() {
-        return referenceID;
-    }
-
-    public void setReferenceID(String referenceID) {
-        this.referenceID = referenceID;
-    }
+//    public String getReferenceID() {
+//        return referenceID;
+//    }
+//
+//    public void setReferenceID(String referenceID) {
+//        this.referenceID = referenceID;
+//    }
 
     public String getAddress() {
         return address;
