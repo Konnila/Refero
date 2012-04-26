e26d50f2560207bd3b71048469a23255b2fb3e0c
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2012-04-26 23:43:57 +0300

    Remove equals method from Author

--- src/main/java/ohtu/refero/models/Author.java
+++ src/main/java/ohtu/refero/models/Author.java
@@ -1,11 +1,8 @@
 package ohtu.refero.models;
 
-import java.io.Serializable;
-import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 @Entity
@@ -13,10 +10,13 @@ public class Author extends JPAObject {
 
     private String firstName;
     private String surName;
+    
     @ManyToMany
     List<Article> articleReferenceList;
+    
     @ManyToMany
     List<Book> bookReferenceList;
+    
     @ManyToMany
     List<Inproceedings> inproceedingsReferenceList;
 
@@ -25,9 +25,6 @@ public class Author extends JPAObject {
         articleReferenceList = new ArrayList<Article>();
         inproceedingsReferenceList = new ArrayList<Inproceedings>();
     }
-
-
-    
     
     public List<Article> getArticleReferenceList() {
         return articleReferenceList;
@@ -67,22 +64,6 @@ public class Author extends JPAObject {
 
     public void setSurName(String surName) {
         this.surName = surName;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        Author auth = (Author) obj;
-        if (auth.getFirstName() == this.firstName && auth.getSurName() == this.surName) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        return hash;
     }
 
     @Override
